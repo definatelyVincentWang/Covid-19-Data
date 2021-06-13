58658a93f49303d9550433d24706c5c27963dc44
Vincent Wang <vinowang921@gmail.com>
2021-06-13 13:28:49 -0400

    Fix grammer mistakes

--- states/make_fig.py
+++ states/make_fig.py
@@ -163,30 +163,29 @@ if cmd == 'US':
     plt.subplot(N,1,4)
     line(date_deaths, "Total deaths per first day of month", "day", "deaths") 
 
+    # line graphs for deaths and cases per day
+    date_cases, date_deaths = unpack_multiple_data("US_Data/unused_dates_data.csv")
+    plt.subplot(N,1,5)
+    line(date_cases, "Total cases per day", "day", "cases")
+    
+    plt.subplot(N,1,6)
+    line(date_deaths, "Total deaths per day", "day", "deaths") 
+
     # gender and age graph
     women_cases, women_deaths, labels = unpack_genders("US_Data/Women_Data.csv")
     men_cases, men_deaths, labels = unpack_genders("US_Data/Men_Data.csv")
 
-    plt.subplot(N,1,5)
+    plt.subplot(N,1,7)
     gender(men_cases, women_cases, labels, "cases")
 
-    plt.subplot(N,1,6)
+    plt.subplot(N,1,8)
     gender(men_deaths, women_deaths, labels, "deaths")
 
     # fatality rate bar graph
     fatality_rate = unpack_data("US_Data/Fatality_Data.csv")
-    plt.subplot(N,1,7)
+    plt.subplot(N,1,9)
     bar(fatality_rate, "Top 20 highest COVID-19 Fatality Rates", "state", "Fatality Rate")    
     plt.xticks(rotation=45)
-    
-    # line graph for deaths and cases per day
-    date_cases, date_deaths = unpack_multiple_data("US_Data/unused_dates_data.csv")
-
-    plt.subplot(N,1,8)
-    line(date_cases, "Total cases per day", "day", "cases")
-
-    plt.subplot(N,1,9)
-    line(date_deaths, "Total deaths per day", "day", "deaths") 
 
     plt.savefig(sys.stdout.buffer)
 
